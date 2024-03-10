13ce869ebbabc1e14f3c54bfcca704bce4a41e69
glace <none>
2024-03-10 19:22:03 -0400

    Prevent crash if no settings json

--- settings/settings.go
+++ settings/settings.go
@@ -5,6 +5,8 @@ import (
 	"entrance1/log"
 	"io/ioutil"
 	"os"
+
+	"go.uber.org/zap"
 )
 
 type (
@@ -23,11 +25,15 @@ func Load() {
 	var settings Settings
 	s, err := os.Open("settings.json")
 	if err != nil {
-		q, _ := json.MarshalIndent(Settings{
+		standard := Settings{
 			Address: ":8000",
-		}, "", "	")
+		}
+		q, _ := json.MarshalIndent(standard, "", "	")
 		ioutil.WriteFile("settings.json", q, os.ModePerm)
-		log.Log.Warn("No settings found! Please configure through the frontend or via settings.json")
+		log.Log.Warn("No valid settings found! Running on default listener -- this can be configured via API or settings.json",
+			zap.String("listener", standard.Address),
+		)
+		State = standard
 	} else {
 		json.NewDecoder(s).Decode(&settings)
 	}
